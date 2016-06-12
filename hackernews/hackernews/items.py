3c2d58b70274027bfcb1f375663956f70c01e3b2
jerodestapa <jerodestapa@gmail.com>
2016-06-12 18:26:34 -0500

    Added crawler

--- hackernews/hackernews/items.py
+++ hackernews/hackernews/items.py
@@ -9,6 +9,6 @@ import scrapy
 
 
 class HackernewsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
     pass
